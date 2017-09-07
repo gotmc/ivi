3cb7a7cbdc7c2329d35cb49b635f54cc302fd935
Matthew Rankin <matthew.d.rankin@gmail.com>
2017-09-07 11:33:11 -0600

    Add dmm/agilent/ag3446x/base.go

---
+++ dmm/agilent/ag3446x/base.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 The ivi developers. All rights reserved.
+// Project site: https://github.com/gotmc/ivi
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
+package ag3446x
+
+import "github.com/gotmc/ivi/dmm"
+
+func (d *Ag3446x) MeasurementFunction() (dmm.MeasurementFunction, error) {
+	f, err := d.QueryString("CONF?\n")
+	return dmm.DCVolts, nil
+}
