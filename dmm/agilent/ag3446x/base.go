c17e53353997d0c38281d6e56ea3591b6661be0b
Matthew Rankin <matthew.d.rankin@gmail.com>
2019-12-04 09:13:10 -0600

    Updates to Agilent DMM driver.

--- dmm/agilent/ag3446x/base.go
+++ dmm/agilent/ag3446x/base.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 The ivi developers. All rights reserved.
+// Copyright (c) 2017-2019 The ivi developers. All rights reserved.
 // Project site: https://github.com/gotmc/ivi
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
@@ -7,6 +7,7 @@ package ag3446x
 
 import "github.com/gotmc/ivi/dmm"
 
+// MeasurementFunction needs a better comment.
 func (d *Ag3446x) MeasurementFunction() (dmm.MeasurementFunction, error) {
 	f, err := d.QueryString("CONF?\n")
 	return dmm.DCVolts, nil
--- dmm/dmm.go
+++ dmm/dmm.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2017-2019 The ivi developers. All rights reserved.
+// Project site: https://github.com/gotmc/ivi
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
 package dmm
 
 // MeasurementFunction provides the defined values for the Measurement Function defined in
