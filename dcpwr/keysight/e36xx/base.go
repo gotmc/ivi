8258217f839f80612b534c10249b9541cd7b0286
Matthew Rankin <matthew.d.rankin@gmail.com>
2021-12-13 12:26:05 -0600

    Prepared for 2022.

--- dcpwr/keysight/e36xx/base.go
+++ dcpwr/keysight/e36xx/base.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021 The ivi developers. All rights reserved.
+// Copyright (c) 2017-2022 The ivi developers. All rights reserved.
 // Project site: https://github.com/gotmc/ivi
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
