29e2b23e3b3807ae33200491fc123de268d2b626
Matthew Rankin <matthew.d.rankin@gmail.com>
2024-04-25 14:30:27 -0500

    Rename files. Add specan/01_specan.go

---
+++ specan/01_specan.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2017-2024 The ivi developers. All rights reserved.
+// Project site: https://github.com/gotmc/ivi
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
+/*
+Package specan provides the defined values, interfaces, structs, methods, and
+enums that are common among all instruments meeting the IVI-4.8: IviSpecAn Class
+Specification, Revision 2.0 dated March 3, 2017.
+
+Files are split based on the class capability groups listed in Table 2-1
+IviSpecAn Group Names in the IVI-4.8 IviSpecAn Class Specification.
+*/
+package specan
