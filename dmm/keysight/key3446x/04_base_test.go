e95b7761aaf51503ae2e7b66b5d9834e501c8581
Matthew Rankin <matthew.d.rankin@gmail.com>
2024-05-02 07:35:45 -0500

    Add tests for key3446x

--- dmm/keysight/key3446x/04_base_test.go
+++ dmm/keysight/key3446x/04_base_test.go
@@ -8,28 +8,68 @@ package key3446x
 import (
 	"testing"
 
+	"github.com/gotmc/ivi"
 	"github.com/gotmc/ivi/dmm"
 )
+
+func TestCreateConfigureVoltageACCommand(t *testing.T) {
+	testCases := []struct {
+		autoRange   dmm.AutoRange
+		rangeValue  float64
+		expected    string
+		expectedErr error
+	}{
+		{dmm.AutoOn, 0.0, "CONF:VOLT:AC AUTO", nil},
+		{dmm.AutoOff, 0.09, "CONF:VOLT:AC 0.1", nil},
+		{dmm.AutoOn, 10.0, "CONF:VOLT:AC AUTO", nil},
+		{dmm.AutoOff, 9.0, "CONF:VOLT:AC 10", nil},
+		{dmm.AutoOff, 10.0, "CONF:VOLT:AC 10", nil},
+		{dmm.AutoOff, 10.001, "CONF:VOLT:AC 100", nil},
+		{dmm.AutoOff, 99.99, "CONF:VOLT:AC 100", nil},
+		{dmm.AutoOff, 100.00, "CONF:VOLT:AC 100", nil},
+		{dmm.AutoOff, 100.001, "CONF:VOLT:AC 1000", nil},
+		{dmm.AutoOff, 999.999, "CONF:VOLT:AC 1000", nil},
+		{dmm.AutoOff, 1000.0, "CONF:VOLT:AC 1000", nil},
+		{dmm.AutoOff, 1000.01, "", ivi.ErrValueNotSupported},
+	}
+	for _, tc := range testCases {
+		got, err := createConfigureVoltageACCommand(tc.autoRange, tc.rangeValue)
+		if err != tc.expectedErr {
+			t.Errorf("wanted err %s / got err %s", tc.expectedErr, err)
+		}
+
+		if got != tc.expected {
+			t.Errorf("wanted %v / got %v", tc.expected, got)
+		}
+	}
+}
 
 func TestDetermineVoltageRange(t *testing.T) {
 	testCases := []struct {
 		autoRange   dmm.AutoRange
 		rangeValue  float64
-		expectedErr error
 		expected    string
+		expectedErr error
 	}{
-		{
-			dmm.AutoOn,
-			0.0,
-			nil,
-			"AUTO",
-		},
+		{dmm.AutoOn, 0.0, "AUTO", nil},
+		{dmm.AutoOff, 0.09, "0.1", nil},
+		{dmm.AutoOn, 10.0, "AUTO", nil},
+		{dmm.AutoOff, 9.0, "10", nil},
+		{dmm.AutoOff, 10.0, "10", nil},
+		{dmm.AutoOff, 10.001, "100", nil},
+		{dmm.AutoOff, 99.99, "100", nil},
+		{dmm.AutoOff, 100.00, "100", nil},
+		{dmm.AutoOff, 100.001, "1000", nil},
+		{dmm.AutoOff, 999.999, "1000", nil},
+		{dmm.AutoOff, 1000.0, "1000", nil},
+		{dmm.AutoOff, 1000.01, "", ivi.ErrValueNotSupported},
 	}
 	for _, tc := range testCases {
 		got, err := determineVoltageRange(tc.autoRange, tc.rangeValue)
 		if err != tc.expectedErr {
 			t.Errorf("wanted err %s / got err %s", tc.expectedErr, err)
 		}
+
 		if got != tc.expected {
 			t.Errorf("wanted %v / got %v", tc.expected, got)
 		}
