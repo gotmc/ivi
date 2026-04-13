63a1ffbc8102ac525adf5a7e8f3f798ddc47b1cd
Matthew Rankin <matthew.d.rankin@gmail.com>
2026-04-13 07:22:59 -0500

    Change Instrument interface to Transport (breaking)

--- ivi.go
+++ ivi.go
@@ -7,23 +7,39 @@ package ivi
 
 import "context"
 
-// Instrument provides the interface required for all IVI Instruments.
-type Instrument interface {
-	ReadContext(ctx context.Context, p []byte) (n int, err error)
-	WriteContext(ctx context.Context, p []byte) (n int, err error)
+// Transport provides the interface required for all IVI Instruments to
+// communicate with test equipment regardless of the transport layer driver
+// used (e.g., usbtmc, asrl, lxi, prologix).
+type Transport interface {
 	Command(ctx context.Context, cmd string, a ...any) error
-	Query(ctx context.Context, s string) (value string, err error)
+	Query(ctx context.Context, cmd string) (value string, err error)
+	ReadBinary(ctx context.Context, p []byte) (n int, err error)
+	WriteBinary(ctx context.Context, p []byte) (n int, err error)
 	Close() error
 }
 
-// Commander provides the interface to send a command to an instrument that is
-// optionally formatted according to a format specifier.
+// Commander provides the interface to send a SCPI/ASCII command with a
+// terminator to an instrument that is optionally formatted according to a
+// format specifier.
 type Commander interface {
 	Command(ctx context.Context, cmd string, a ...any) error
 }
 
-// Querier provides the interface to query an instrument using a given command
-// and then provide the resultant string.
+// Querier provides the interface to query an instrument using a given
+// SCPI/ASCII command that reads until the appropriate terminator and then
+// provides the resultant string.
 type Querier interface {
-	Query(ctx context.Context, s string) (value string, err error)
+	Query(ctx context.Context, cmd string) (value string, err error)
+}
+
+// BinaryReader provides the interface to read binary data without
+// terminator interpretation.
+type BinaryReader interface {
+	ReadBinary(ctx context.Context, p []byte) (n int, err error)
+}
+
+// BinaryWriter provides the interface to write binary data without adding a
+// terminator.
+type BinaryWriter interface {
+	WriteBinary(ctx context.Context, p []byte) (n int, err error)
 }
